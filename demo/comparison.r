38819f2ff239c28aca37b60ed94a75385598115c
baptiste <baptiste.auguie@gmail.com>
2018-08-22 09:32:16 +1200

    die

--- demo/comparison.r
+++ demo/comparison.r
@@ -5,14 +5,14 @@ require(ggplot2)
 
 ## ----comparison, echo=TRUE------------------------------------
 
-gold <- epsAu(seq(300, 1000))
+gold <- epsAg(seq(200, 600))
 
-test <- with(gold, mie_bh(wavelength, epsilon, radius = 40,
-                     medium = 1.33, efficiency = FALSE))
+test <- with(gold, mie_bh(wavelength, epsilon, radius = 25,
+                     medium = 1.0, efficiency = FALSE))
 
 
-cross_sections <- with(gold, mie(wavelength, epsilon, radius=40, 
-                                 medium=1.33, efficiency=FALSE))
+cross_sections <- with(gold, mie(wavelength, epsilon, radius=25, 
+                                 medium=1.0, efficiency=FALSE, nmax = 4))
 cross_sections2 <- data.frame(wavelength=gold[, 1], scattering = test[, 3], 
                               absorption=test[, 4])
 
@@ -21,10 +21,14 @@ m2 <- melt(cross_sections2, id=1, measure=c("scattering", "absorption"))
 
 p <- 
 ggplot(m, aes(wavelength, value,  group=variable))+
-  geom_line(aes(colour=variable),alpha=0.5, size=1.5) +
-  geom_line(aes(colour=variable), size=1.5,  data=m2, linetype=2) +
-  labs(x="wavelength /nm", y=expression(sigma/(pi*a^2)), colour="") +
+  geom_line(aes(colour=variable),alpha=0.5, size=1.0) +
+  geom_line(aes(colour=variable), size=1.0,  data=m2, linetype=2) +
+  labs(x="wavelength /nm", y=expression(sigma), colour="") +
   scale_fill_brewer(palette="Pastel2")+
   scale_colour_brewer(palette="Set2")
 
 p
+
+
+write.table(file='mie_25nm_Ag.txt', cross_sections, row.names = F)
+system("open .")
