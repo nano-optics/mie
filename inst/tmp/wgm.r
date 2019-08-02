e1b16629169a95558ba9be056d80372fa61cd701
baptiste <baptiste.auguie@gmail.com>
2019-08-02 17:22:24 +1200

    Update wgm.r

--- inst/tmp/wgm.r
+++ inst/tmp/wgm.r
@@ -5,30 +5,34 @@ require(ggplot2)
 
 ## ----comparison, echo=TRUE------------------------------------
 
-wavelength <- seq(300, 1000)
-gold <- epsAu(wavelength)
+wavelength <- seq(500, 600, length=2e3)
+mat <- data.frame(wavelength = wavelength, epsilon = 1.5^2 +0i)
 
-model <- function(a=30, d=10){
-test <- mie_bh(wavelength, epsilon.core=gold$epsilon, epsilon.coating = 1.5^2,
-               thickness = d, radius = a, medium = 1.33, efficiency = TRUE)
-
-cross_sections <- data.frame(wavelength=gold[, 1], scattering = test[, 3], 
-                              absorption=test[, 4], extinction=test[, 2])
-
-m <- melt(cross_sections, id=1, measure=c("scattering", "absorption", "extinction"))
+model <- function(a=30, d=0, medium=1.33){
+  test <- mie_bh(wavelength, epsilon.core=mat$epsilon, epsilon.coating = 1.5^2,
+                 thickness = d, radius = a, medium = medium, efficiency = FALSE)
+  
+  cross_sections <- data.frame(wavelength=mat[, 1], scattering = test[, 3], 
+                               absorption=test[, 4], extinction=test[, 2])
+  
+  # m <- melt(cross_sections, id=1, measure=c("scattering", "absorption", "extinction"))
+  m <- melt(cross_sections, id=1, measure=c( "extinction"))
 }
-params <- expand.grid(a=seq(5, 40, by=5), d=seq(100, 150, by=5))
-all <- mdply(params, model, .progress='text')
+
+params <- expand.grid(a=seq(500, 1500, by=500))
+# params <- expand.grid(a=seq(5000, 8000, by=500))
+all <- mdply(params, model, .progress='text', medium=1.0)
 p <- 
-ggplot(all, aes(wavelength, value, linetype=variable))+
-  facet_grid(.~a) +
-  geom_line(aes(colour=d,group=interaction(variable,d))) +
-  labs(x="wavelength /nm", y=expression(sigma/(pi*a^2)), colour="") +
+  ggplot(all, aes(wavelength, value, linetype=variable))+
+  facet_wrap(~a, scales="free") +
+  geom_line(aes(colour=factor(a),group=interaction(variable))) +
+  labs(x="wavelength /nm", y=expression(sigma[ext]/nm^2), colour="radius /nm") +
   scale_fill_brewer(palette="Pastel2") +
-  scale_color_viridis_c()
+  scale_color_brewer(palette="Set1")
 
 p
 
+ggsave("glassinair.pdf",width=10,height=3)
 # s <- susceptibility(2, 1, 3)
 # s$G
 # -0.00814193+0.08986456i -4.023e-06+2.005721e-03i -9.1e-10+3.009717e-05i
